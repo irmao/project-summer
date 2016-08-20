696256dee3344042870bca89a2054314cba43186
irmao <v.fernandesdias@gmail.com>
2016-08-20 15:18:51 -0300

    Permission to access Azure server added

--- source/ProjectSummer/www/services/ConnectionService.js
+++ source/ProjectSummer/www/services/ConnectionService.js
@@ -13,6 +13,7 @@
        */
         executeServerRequest : function (url) {
         var hostaddr = 'http://localhost';
+        //var hostaddr = 'http://project-summer.azurewebsites.net/server';
         var fulladdr = hostaddr + url + '&callback=JSON_CALLBACK';
         return $http.jsonp(fulladdr);
       }
